5b2bba03416a5f4ec429ff85bd6d1c4b540e0cfa
rsung <rsung@student.42.fr>
2022-12-23 18:22:40 +0100

    html-css

--- srcs/config/Server.d
+++
@@ -1,8 +0,0 @@
-srcs/config/Server.o: srcs/config/Server.cpp srcs/config/Server.hpp \
-  srcs/config/Location.hpp srcs/config/Keys.hpp
-
-srcs/config/Server.hpp:
-
-srcs/config/Location.hpp:
-
-srcs/config/Keys.hpp:
