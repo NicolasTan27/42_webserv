4f3e77af14abf91dd00bd906b42bba7436efc811
rsung <rsung@student.42.fr>
2022-12-28 12:06:10 +0100

    css delete upload

--- data/cgi/php/delete.php
+++ data/cgi/php/delete.php
@@ -21,16 +21,18 @@ parse_str($entityBody, $BODY);
 <head>
 	<meta charset="UTF-8">
 	<title>Form 42 Webserv</title>
+	<link href="/cgi/php/css/delete.css" rel="stylesheet">
 </head>
 
 <body>
+<style> body {background-color: green;} </style>
 <div> <a href="/cgi/php/main.php">Main</a> <a href="/cgi/php/upload.php">Upload</a> <a href="/cgi/php/post.php">Post</a> <a href="/cgi/php/delete.php">Delete</a> </div>
 <h1>File Destroyer</h1>
 
 <form action="delete.php" method="post" onsubmit="test()">
- 	<label for="file">Select a file to delete:</label>
+ 	<label for="file"><p class="Select">Select a file to delete:</label>
   	<select name="file" id="file">
-  		<option value="">Select a file</option>
+  		<option value="">Select a file</option></p>
 		<?php
 			$dir = scandir($wdir);
 			foreach ($dir as $doc)
