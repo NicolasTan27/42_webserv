1975f68f5133200532e0c17896dcf672fe35253f
Soyoung JUNG <sojung@e2r7p10.clusters.42paris.fr>
2022-12-29 15:59:51 +0100

    multiple ports in Webserser::start()

--- srcs/webserver/Webserver.cpp
+++ srcs/webserver/Webserver.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Webserver.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntan <ntan@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sojung <sojung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 17:18:15 by ntan              #+#    #+#             */
-/*   Updated: 2022/12/28 18:11:07 by ntan             ###   ########.fr       */
+/*   Updated: 2022/12/29 15:57:26 by sojung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,37 @@ int	Webserver::start()
 {
 	SocketInfo	test(this->config);
 	
-	int port[2];
-	port[0] = 8000;
-	port[1] = 8002;
-	// for (int i = 0; i < 2; i++)
-		test.add_socket(INADDR_ANY, port[0]);
-		test.add_socket(INADDR_ANY, port[1]);
+	// int port[2];
+	// port[0] = 8000;
+	// port[1] = 8002;
+	// // for (int i = 0; i < 2; i++)
+	// 	test.add_socket(INADDR_ANY, port[0]);
+	// 	test.add_socket(INADDR_ANY, port[1]);
 	
-	// test.init_master_set();
+	// // test.init_master_set();
+	// test.set_timeout();
+	// test.server_loop();
+
+	std::vector<int> ports;
+	int port;
+
+	for (size_t i = 0; i < config.servers_count; i++)
+	{
+		port = atoi(config.servers[i].listen.values[1].c_str());
+		std::cout << GREEN << "PORT NUM : " << port << NOR << std::endl;  
+		if (std::find(ports.begin(), ports.end(), port) == ports.end())
+		{
+			ports.push_back(port);
+			std::cout << "push backed port : " << port << std::endl;
+		}
+		else
+		{
+			std::cout << "Same ports detected in the configuration file" << std::endl;
+			return (1);
+		}
+	}
+	test.socket_master(ports);
+	test.listen_socket();
 	test.set_timeout();
 	test.server_loop();
 
